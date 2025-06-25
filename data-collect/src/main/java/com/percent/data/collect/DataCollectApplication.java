86987afe247b9dbead0f3ed5a22136a0a0ae464b
turui <turui@simright.com>
2025-06-25 21:59:34 +0800

    feat: 增加data-collect模块.

---
+++ data-collect/src/main/java/com/percent/data/collect/DataCollectApplication.java
@@ -0,0 +1,13 @@
+package com.percent.data.collect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCollectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataCollectApplication.class, args);
+    }
+
+}
